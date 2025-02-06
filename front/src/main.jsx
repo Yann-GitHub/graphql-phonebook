9bf2a416c898d1cb1447a55e04fa0ac030cf07f9
Yann <ybarlet@gmail.com>
2025-02-06 09:47:00 +0100

    set up backend with Apollo Server and GraphQL

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
