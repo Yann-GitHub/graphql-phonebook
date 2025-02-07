bb90d88b17ab75dfae1addec4ecea32b25b27f31
Yann <ybarlet@gmail.com>
2025-02-07 10:30:11 +0100

    integrate Apollo Client with React app

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -12,7 +12,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -37,4 +42,8 @@ client.query({ query }).then((response) => {
   console.log(response.data);
 });
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
