df7d5f91202e2861617a01e4aa0c8d096f182ca2
Yann <ybarlet@gmail.com>
2025-02-13 18:30:41 +0100

    add error notification handling

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,8 +2,11 @@ import { useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import { ALL_PERSONS } from "./queries";
+import { useState } from "react";
+import Notify from "./components/Notify";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
   // Here we use the useQuery hook to send the query to the server.
   // const result = useQuery(ALL_PERSONS);
 
@@ -19,10 +22,18 @@ const App = () => {
     return <div>loading...</div>;
   }
 
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };
--- front/src/components/PersonForm.jsx
+++ front/src/components/PersonForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { ALL_PERSONS, CREATE_PERSON } from "../queries";
 
-const PersonForm = () => {
+const PersonForm = ({ setError }) => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [street, setStreet] = useState("");
@@ -12,6 +12,10 @@ const PersonForm = () => {
   // Cache is updated automatically.
   const [createPerson] = useMutation(CREATE_PERSON, {
     refetchQueries: [{ query: ALL_PERSONS }],
+    onError: (error) => {
+      const messages = error.graphQLErrors.map((e) => e.message).join("\n");
+      setError(messages);
+    },
   });
 
   const submit = (event) => {
@@ -30,28 +34,28 @@ const PersonForm = () => {
       <h2>create new</h2>
       <form onSubmit={submit}>
         <div>
-          name{" "}
+          name
           <input
             value={name}
             onChange={({ target }) => setName(target.value)}
           />
         </div>
         <div>
-          phone{" "}
+          phone
           <input
             value={phone}
             onChange={({ target }) => setPhone(target.value)}
           />
         </div>
         <div>
-          street{" "}
+          street
           <input
             value={street}
             onChange={({ target }) => setStreet(target.value)}
           />
         </div>
         <div>
-          city{" "}
+          city
           <input
             value={city}
             onChange={({ target }) => setCity(target.value)}
